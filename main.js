b1a90d683c8b1188ff011a738f76b8f0ef8ea9a1
BoladjiDansou1 <114788935+BoladjiDansou1@users.noreply.github.com>
2023-04-14 18:45:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+}
+function setup (){
+    canvas= createCanvas(300,300);
+    canvas.position(500,200);
+    //video = createCapture(VIDEO);
+//video.hide();
+
+}
+
+function draw() {
+    //image(video,0,0,640,480);
+   
+}
+
+function take_snapshot(){
+     save('myFilterImage.png'); 
+    }
